84055a36e0f37526fa0a38cf9f903cff491f5395
kimjueun028 <jueuniiiju@naver.com>
2026-02-08 16:29:40 +0900

    chore-7: align job data schema to test

--- src/job_finder_rec/data/jobs/job_adapter.py
+++ src/job_finder_rec/data/jobs/job_adapter.py
@@ -3,4 +3,4 @@ from job_finder_rec.recommender.types import JobPosting
 
 
 def adapt_jobs(raw_jobs: List[Dict[str, Any]]) -> List[JobPosting]:
-    return [JobPosting.from_dict(d, fallback_id=f"job_{i+1}") for i, d in enumerate(raw_jobs)]
+    return [JobPosting.from_dict(d) for i, d in enumerate(raw_jobs)]
